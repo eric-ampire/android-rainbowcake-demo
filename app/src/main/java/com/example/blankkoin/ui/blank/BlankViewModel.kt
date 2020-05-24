3c64d9c4efddda3999da0efa3667047880fd1916
Márton Braun <braunmarci@gmail.com>
2020-05-24 20:18:43 +0200

    Update to RainbowCake 0.7.0

--- app/src/main/java/com/example/blankkoin/ui/blank/BlankViewModel.kt
+++ app/src/main/java/com/example/blankkoin/ui/blank/BlankViewModel.kt
@@ -1,10 +1,10 @@
 package com.example.blankkoin.ui.blank
 
-import co.zsmb.rainbowcake.base.JobViewModel
+import co.zsmb.rainbowcake.base.RainbowCakeViewModel
 
 class BlankViewModel(
     private val blankPresenter: BlankPresenter
-) : JobViewModel<BlankViewState>(Loading) {
+) : RainbowCakeViewModel<BlankViewState>(Loading) {
 
     fun load() = execute {
         viewState = BlankReady(blankPresenter.getData())
